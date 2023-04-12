b4acb370538f21d7e8a26eb44170b3fa95061309
JohanGuerra <davidjohan00@gmail.com>
2023-04-11 22:15:50 -0500

    Validacion de campos vacios

--- js/programa.js
+++ js/programa.js
@@ -2,15 +2,22 @@ const usuarioField = document.querySelector("[name=usuario]");
 const contraseñaField = document.querySelector("[name=contraseña]");
 const correoField = document.querySelector("[name=correo]");
 
-
-usuarioField.addEventListener("blur",function(e){
+const validacionCampoVacio = (e) =>{
     const field = e.target;
     const fieldValue = e.target.value;
-    if(fieldValue.length === 0){
-        field.parentElement.insertAdjacentHTML(
-            "beforeend",
-            `<span class="error">Error</span>`
-        )
+    if (fieldValue.length === 0) {
+        field.classList.add("invalid")
+        field.nextElementSibling.classList.add("error")
+        field.nextElementSibling.innerText = `Se necesita ${field.name}`
+    } else { 
+        field.classList.remove("invalid")
+        field.nextElementSibling.classList.remove("error")
+        field.nextElementSibling.innerText = ""
     }
     
-})
+}
+
+
+usuarioField.addEventListener("blur", validacionCampoVacio);
+contraseñaField.addEventListener("blur", validacionCampoVacio);
+correoField.addEventListener("blur", validacionCampoVacio);
