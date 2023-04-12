e8730bcb425ca9f3e78c716f5c2bb3005a45a141
JohanGuerra <davidjohan00@gmail.com>
2023-04-11 23:04:33 -0500

    casi termino

--- js/programa.js
+++ js/programa.js
@@ -2,13 +2,13 @@ const usuarioField = document.querySelector("[name=usuario]");
 const contraseñaField = document.querySelector("[name=contraseña]");
 const correoField = document.querySelector("[name=correo]");
 
-const validacionCampoVacio = (e) =>{
+const validacionCampoVacio = ( mensaje,e,) =>{
     const field = e.target;
     const fieldValue = e.target.value;
-    if (fieldValue.length === 0) {
+    if (fieldValue.trim().length === 0) {
         field.classList.add("invalid")
         field.nextElementSibling.classList.add("error")
-        field.nextElementSibling.innerText = `Se necesita ${field.name}`
+        field.nextElementSibling.innerText = mensaje;
     } else { 
         field.classList.remove("invalid")
         field.nextElementSibling.classList.remove("error")
@@ -17,7 +17,23 @@ const validacionCampoVacio = (e) =>{
     
 }
 
+const validarCorreos = e => {
+    const field = e.target;
+    const fieldValue = e.target.value;
+    const regex = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
+    if (fieldValue.trim().length > 5 && !regex.test(fieldValue)) {
+        field.classList.add("invalid")
+        field.nextElementSibling.classList.add("error")
+        field.nextElementSibling.innerText = "Por favor dijite un correo valido";
+    } else { 
+        field.classList.remove("invalid")
+        field.nextElementSibling.classList.remove("error")
+        field.nextElementSibling.innerText = ""
+    }
+}
 
-usuarioField.addEventListener("blur", validacionCampoVacio);
-contraseñaField.addEventListener("blur", validacionCampoVacio);
-correoField.addEventListener("blur", validacionCampoVacio);
+usuarioField.addEventListener("blur", (e)=> validacionCampoVacio("Se necesita el nombre", e));
+contraseñaField.addEventListener("blur", (e)=> validacionCampoVacio("Se necesita la contraseña", e));
+correoField.addEventListener("blur", (e) => validacionCampoVacio("Se necesita el correo", e));
+
+correoField.addEventListener("input", validarCorreos)
